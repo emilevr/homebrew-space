1cc67e81de22e6284a6105d8856f46affd262ef2
Emile van Reenen <9017146+emilevr@users.noreply.github.com>
2023-11-11 09:56:22 +0200

    Update space to version 1.0.0

--- Formula/space.rb
+++ Formula/space.rb
@@ -1,19 +1,13 @@
 class Space < Formula
   desc "Analyze and clean your disk space (powered by Rust)"
   homepage "https://github.com/emilevr/space"
-  url "https://github.com/emilevr/space/archive/refs/tags/0.1.6.tar.gz"
-  sha256 "df839731ffd6904df06c347e56f5b6052cbe0f42a0746939bcf93da90f702531"
+  url "https://github.com/emilevr/space/archive/refs/tags/1.0.0.tar.gz"
+  sha256 "7cd3bd5b4131d6b6f6140b009f8ef60bd665eb8ebd22ddfa6514cbac5ed2e7ff"
   license "MIT"
 
   livecheck do
     url :stable
     strategy :github_latest
-  end
-
-  bottle do
-    root_url "https://github.com/emilevr/homebrew-space/releases/download/space-0.1.6"
-    sha256 cellar: :any_skip_relocation, ventura:      "7b8b5abda1485437e74899fbc1201ce90c1f4e02d91c3fb7b0d4db6ca87e8f95"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "2b1fded1caa38dba91321e20cf301b9e8aebd0790d916b144fcbea4a6c5bdbc5"
   end
 
   depends_on "rust" => :build
