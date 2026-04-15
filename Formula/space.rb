0e5769eb3cefeef1022f149af33815ddccfb73dc
Emile van Reenen <9017146+emilevr@users.noreply.github.com>
2026-04-15 22:06:04 +0000

    space: add 2.0.0 bottle.

--- Formula/space.rb
+++ Formula/space.rb
@@ -10,6 +10,13 @@ class Space < Formula
     strategy :github_latest
   end
 
+  bottle do
+    root_url "https://github.com/emilevr/homebrew-space/releases/download/space-2.0.0"
+    rebuild 1
+    sha256 cellar: :any_skip_relocation, arm64_sequoia: "d0607201e8b202938bd4b332282c320a3c737435d3785d36df673822865995e3"
+    sha256 cellar: :any_skip_relocation, x86_64_linux:  "4b2a263ca90416cfe0a604d4c9e5b483fd7c6dae95e92965ce167b28c98ee535"
+  end
+
   depends_on "rust" => :build
 
   def install
