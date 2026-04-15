c62a1fbf6b2cacfef1df6c0e72ef8073a72fd556
Emile van Reenen <9017146+emilevr@users.noreply.github.com>
2026-04-15 22:31:40 +0200

    Publish Space v2.0.0
    Closes #6.
    
    Signed-off-by: Emile van Reenen <9017146+emilevr@users.noreply.github.com>

--- Formula/space.rb
+++ Formula/space.rb
@@ -1,19 +1,13 @@
 class Space < Formula
   desc "Analyze and clean your disk space (powered by Rust)"
   homepage "https://github.com/emilevr/space"
-  url "https://github.com/emilevr/space/archive/refs/tags/1.0.1.tar.gz"
-  sha256 "e9b4e61f962ab9f99d90f3498eecf55e6db9003f5ed8d5887f2fe0929cc3d0ab"
+  url "https://github.com/emilevr/space/archive/refs/tags/2.0.0.tar.gz"
+  sha256 "d38ecb4a5cfbed72a5c1b426b1c990e492f5edc11b9ae6e2681a7a5425ec4b2b"
   license "MIT"
 
   livecheck do
     url :stable
     strategy :github_latest
-  end
-
-  bottle do
-    root_url "https://github.com/emilevr/homebrew-space/releases/download/space-1.0.1"
-    sha256 cellar: :any_skip_relocation, ventura:      "0418ede6846987a88e40c946f6e2dc42c0df7e1a09c3c8b16066280302378d93"
-    sha256 cellar: :any_skip_relocation, x86_64_linux: "18069c9f226cbe9b2a4a9aa1ea58fcbe77ca41ac27e6ab6b5c33f1fffab8faec"
   end
 
   depends_on "rust" => :build
